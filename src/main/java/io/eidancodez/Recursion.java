9141e75be199f9486eb57bf8357cfd44cf5c7326
Eidan <eidankhan.student.muet@gmail.com>
2021-12-15 16:21:14 +0500

    Reverse string using recursion

--- src/main/java/io/eidancodez/Recursion.java
+++ src/main/java/io/eidancodez/Recursion.java
@@ -1,14 +1,22 @@
 package io.eidancodez;
 
+import java.util.Vector;
+
 public class Recursion {
+    private static String reverseOfText;
+
+    
     public static void main(String[] args) {
         int n = 5;
         System.out.print("Sum of first "+n+" numbers ");
         System.out.println(" is "+sum(n));
+        System.out.println("Reverse:"+reverse("My name is Khan"));
+       
+        
+       
     }
 
-    // Print some of first n natural numbers 
-     
+    // Print sum of first n natural numbers 
     public static int sum(int number){
         if(number > 0)
         {
@@ -20,5 +28,11 @@ public class Recursion {
         }
     }
 
+    public static String reverse(String text){
+        if(text.isEmpty())
+            return text;
+        else
+            return reverse(text.substring(1))+text.charAt(0);
+    }
    
 }
